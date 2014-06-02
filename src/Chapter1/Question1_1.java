bb804c0fc0683edd312941e37d7ae15b9fcdd405
biubiuX <zxiong3@asu.edu>
2014-06-02 15:32:43 -0700

    update another solution

--- src/Chapter1/Question1_1.java
+++ src/Chapter1/Question1_1.java
@@ -22,9 +22,25 @@ public class Question1_1 {
 		return true;
 	}
 	
+	public static boolean isUnique2(String str){
+		if (str.length()>256){
+			return false;
+		}
+		
+		boolean[] charset = new boolean[256];
+		for (int i=0;i<str.length();i++){
+			int val = str.charAt(i);
+			if(charset[val]){
+				return false;
+			}
+			charset[val]=true;
+		}
+		return true;
+	}
 	public static void main(String args[]){
 		String test = "Cracking the Coding Interview";
-		System.out.print(isUnique(test));
+		System.out.println(isUnique(test));
+		System.out.println(isUnique2(test));
 	}
 	
 }
